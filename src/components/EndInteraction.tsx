f352d52e8fbcf21a129d3c9065c63860687c617d
darksun27 <sbsiddharth@gmail.com>
2021-01-23 03:10:17 +0530

    add UFL qualitrics

--- src/components/EndInteraction.tsx
+++ src/components/EndInteraction.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@material-ui/core";
 
 export const EndInteraction = ():JSX.Element => {
+    const handleClick = () => {
+        var win = window.open('https://ufl.qualtrics.com/jfe/form/SV_3slmjAOwipia4Em','_blank');
+        win?.focus();
+    }
     return (
-        <Button variant="contained" color="secondary" size="large" style={{minWidth:"25%", marginRight:"20px"}}>
+        <Button variant="contained" color="secondary" size="large" style={{minWidth:"25%", marginRight:"20px"}} onClick={handleClick}>
             END INTERACTION
         </Button>
     )
